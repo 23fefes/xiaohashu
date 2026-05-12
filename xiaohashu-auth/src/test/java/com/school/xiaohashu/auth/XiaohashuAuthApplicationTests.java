f42b69db6e130433c63c749725cc2be68f296938
23fefes <19567263342@163.com>
2026-05-12 15:37:40 +0800

    4.4

--- xiaohashu-auth/src/test/java/com/school/xiaohashu/auth/XiaohashuAuthApplicationTests.java
+++ xiaohashu-auth/src/test/java/com/school/xiaohashu/auth/XiaohashuAuthApplicationTests.java
@@ -1,13 +1,40 @@
 package com.school.xiaohashu.auth;
 
+import com.school.framework.common.util.JsonUtils;
+import com.school.xiaohashu.auth.domain.dataobject.UserDO;
+import com.school.xiaohashu.auth.domain.mapper.UserDOMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 @SpringBootTest
+@Slf4j
 class XiaohashuAuthApplicationTests {
 
+    @Resource
+    private UserDOMapper userDOMapper;
+
+    /**
+     * 测试插入数据
+     */
     @Test
-    void contextLoads() {
+    void testInsert() {
+        UserDO userDO = UserDO.builder()
+                .username("犬小哈")
+                .createTime(LocalDateTime.now())
+                .updateTime(LocalDateTime.now())
+                .build();
+
+        userDOMapper.insert(userDO);
     }
 
+    @Test
+    void testSelect() {
+        // 查询主键 ID 为 4 的记录
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1L);
+        log.info("User: {}", JsonUtils.toJsonString(userDO));
+    }
 }
+
