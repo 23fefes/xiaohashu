d11b29e52a53752a5e8022600cb8c7570a6ac029
23fefes <19567263342@163.com>
2026-05-13 17:04:06 +0800

    5.3

--- xiaohashu-auth/src/main/java/com/school/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/school/xiaohashu/auth/controller/TestController.java
@@ -1,12 +1,10 @@
 package com.school.xiaohashu.auth.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.school.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.school.framework.common.response.Response;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -14,24 +12,20 @@ import java.time.LocalDateTime;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    @ApiOperationLog(description = "测试接口")
-    public Response<String> test() {
-        return Response.success("Hello, 犬小哈专栏");
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
     }
 
-    @GetMapping("/test2")
-    @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2() {
-        return Response.success(User.builder()
-                .nickName("犬小哈")
-                .createTime(LocalDateTime.now())
-                .build());
-    }
-    @PostMapping("/test3")
-    @ApiOperationLog(description = "测试接口3")
-    public Response<User> test3(@RequestBody @Validated User user) {
-        return Response.success(user);
+    // 查询登录状态，浏览器访问： http://localhost:8080/user/isLogin
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
     }
 }
 
