d739e5d4113bedf53cc6f4155cf258c430a2f2bb
23fefes <19567263342@163.com>
2026-05-22 14:45:15 +0800

    14end

---
+++ xiaohashu-note/xiaohashu-note-biz/src/main/java/com/school/xiaohashu/note/biz/domain/mapper/TopicDOMapper.java
@@ -0,0 +1,20 @@
+package com.school.xiaohashu.note.biz.domain.mapper;
+
+import com.school.xiaohashu.note.biz.domain.dataobject.TopicDO;
+
+public interface TopicDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TopicDO record);
+
+    int insertSelective(TopicDO record);
+
+    TopicDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(TopicDO record);
+
+    int updateByPrimaryKey(TopicDO record);
+
+    String selectNameByPrimaryKey(Long id);
+
+}
