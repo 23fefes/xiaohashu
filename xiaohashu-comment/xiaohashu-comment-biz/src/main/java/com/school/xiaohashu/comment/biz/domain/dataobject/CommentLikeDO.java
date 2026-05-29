77ec71d73761d3336229037533f0074efebff206
23fefes <19567263342@163.com>
2026-05-29 08:37:09 +0800

    24end

---
+++ xiaohashu-comment/xiaohashu-comment-biz/src/main/java/com/school/xiaohashu/comment/biz/domain/dataobject/CommentLikeDO.java
@@ -0,0 +1,23 @@
+package com.school.xiaohashu.comment.biz.domain.dataobject;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CommentLikeDO {
+    private Long id;
+
+    private Long userId;
+
+    private Long commentId;
+
+    private Date createTime;
+
+}
